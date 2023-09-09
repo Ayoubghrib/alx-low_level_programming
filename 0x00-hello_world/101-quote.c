3805e4d57eb69c38478f26cccc5a3965e49334bb
Ayoubghrib <wacayoub582@gmail.com>
2023-09-09 05:20:10 -0700

    comm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
-#include <unistd.h>
+#!/bin/bash
+gcc -S -masm=intel $CFILE
 
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-    /* Utilisation de la fonction write pour écrire directement sur la sortie standard d'erreur (stderr). */
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

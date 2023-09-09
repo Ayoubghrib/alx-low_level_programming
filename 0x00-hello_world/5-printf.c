335508dd0d3afb9bd7ce9bf29cd08c1a209675ee
Ayoubghrib <wacayoub582@gmail.com>
2023-09-09 05:05:35 -0700

    tesko

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+ * main - prints the phrase:
+ * with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Seccess)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of 
-art,\n
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 

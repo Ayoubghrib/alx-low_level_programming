dbfcff951f751a124dca67e2920d98047520d294
Ayoubghrib <wacayoub582@gmail.com>
2023-09-09 05:27:31 -0700

    paw

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,17 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print a phrase to the standard error.
+ *
+ * Return: retuns 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
 
 
 

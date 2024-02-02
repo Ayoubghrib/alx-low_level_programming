de51ca947986a85427fd73fda3f88bb8cfba3652
Ayoubghrib <Ayoubgharib30@gmail.com>
2024-02-02 06:31:39 -0800

    0x18

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://raw.github.com/AyoubGharib/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD="/tmp/libmmw.so"
+export LD_PRELOAD="$PWD/../libgiga.so"
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef main_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -25,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
